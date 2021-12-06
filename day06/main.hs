feeb9eada4a5a1337f9c0c95f6491cdbb3bd0139
Katie Dsouza <katiedsouza.13@gmail.com>
2021-12-06 10:06:58 -0500

    [Day 6] Solution

--- day06/main.hs
+++ day06/main.hs
@@ -1,44 +1,34 @@
 import System.Environment
 import Data.List
-import Data.Ord
-import Debug.Trace
 import Data.List.Split
+import Data.Map
 
-decreaseTimer :: Int -> [Int]
-decreaseTimer 0 = [6, 8]
-decreaseTimer t = [t - 1]
+-- FishMap maps the timer value to the number of fishes with that timer value
+type FishMap = Map Int Int
 
-decreaseTimerWeek :: Int -> [Int]
-decreaseTimerWeek 0 = [0, 2]
-decreaseTimerWeek 1 = [1, 3]
-decreaseTimerWeek 2 = [2, 4]
-decreaseTimerWeek 3 = [3, 5]
-decreaseTimerWeek 4 = [4, 6]
-decreaseTimerWeek 5 = [5, 7]
-decreaseTimerWeek 6 = [6, 8]
-decreaseTimerWeek 7 = [0]
-decreaseTimerWeek 8 = [1]
+getNextTimerValueForKey :: FishMap -> Int -> Int
+getNextTimerValueForKey values 8 = Data.Map.findWithDefault 0 0 values
+getNextTimerValueForKey values 6 = Data.Map.findWithDefault 0 7 values + Data.Map.findWithDefault 0 0 values
+getNextTimerValueForKey values n = Data.Map.findWithDefault 0 (n+1) values
 
-dt remaining value
-    | remaining `mod` 7 == 0 = map (f value) [0..] !! remaining
-    | otherwise = error "No!"
-    where f n 0 = [n]
-          f 7 r = dt (r-7) 0
-          f 8 r = dt (r-7) 1
-          f n r = dt (r-7) n ++ dt (r-7) (n+2)
+decreaseTimers :: FishMap -> FishMap
+decreaseTimers fishMap = Data.Map.mapWithKey (\key count -> getNextTimerValueForKey fishMap key) fishMap
+
+emptyFishMap :: FishMap
+emptyFishMap = Data.Map.fromList (zip [0..] (replicate 9 0))
+
+runSimulation :: FishMap -> Int -> FishMap
+runSimulation fishMap days = iterate decreaseTimers fishMap !! days
 
 main :: IO ()
 main = do
     (fileName:_) <- getArgs
     contents <- readFile fileName
-    let timers = (map read . splitOn "," . head . lines) contents :: [Int]
-    let timersAligned = foldl (\x i -> concatMap decreaseTimer x) timers [1..4]
-    -- print $ timersAligned
-    let fn = dt 252
-    let result = concatMap fn timersAligned
-    print $ length result
+    let parsedContents = (Data.List.map read . splitOn "," . head . lines) contents :: [Int]
 
-    -- print $ decreaseTimerX 14 3
+    let inputFishMap = Data.Map.fromList $ (Data.List.map (\values -> (head values, length values)) . group . sort) parsedContents
+    let fishMap = Data.Map.unionWith (+) inputFishMap emptyFishMap
 
-    -- print $ concatMap (decreaseTimerX 7) [3,4,3,1,2]
-
+    let resultingFishMap = runSimulation fishMap 256
+    let result = Data.Map.foldl (+) 0 resultingFishMap
+    print result
