ca57b8297f432252c428ce8060856a75270c3374
ksdsouza <katie.dsouza+git@carta.com>
2021-12-08 18:54:23 -0500

    [Day 08] more wip

--- day08/main.hs
+++ day08/main.hs
@@ -7,7 +7,6 @@ splitNote :: [String] -> ([String], [String])
 splitNote values = (take 10 values, drop 11 values)
 
 
-
 is1 :: String -> Bool
 is1 = (2 ==) . length
 
@@ -56,7 +55,7 @@ data Stencil = Stencil {
     bottomLeft :: String,
     bottomRight :: String,
     bottom :: String
-} deriving (Show)
+} deriving (Show, Eq)
 
 emptyStencil :: Stencil
 emptyStencil = Stencil ['a'..'g'] ['a'..'g'] ['a'..'g'] ['a'..'g'] ['a'..'g'] ['a'..'g'] ['a'..'g']
@@ -68,6 +67,41 @@ filterStencil stencil word
     | is4 word = apply4 word stencil
     | is7 word = apply7 word stencil
     | otherwise = stencil
+
+allStencils :: Stencil -> [Stencil]
+allStencils stencil@(Stencil [t] [l] [r] [m] [bl] [br] [b]) = if (validStencil stencil) then [stencil] else []
+allStencils stencil@(Stencil (t1:_:_) _ _ _ _ _ _) =
+    concatMap (\y -> allStencils (stencil {top = [y]})) (top stencil)
+allStencils stencil@(Stencil _ (l1:_:_) _ _ _ _ _) =
+    concatMap (\y -> allStencils (stencil {left = [y]})) (left stencil)
+allStencils stencil@(Stencil _ _ (r1:_:_) _ _ _ _) =
+    concatMap (\y -> allStencils (stencil {right = [y]})) (right stencil)
+allStencils stencil@(Stencil _ _ _ (m1:_:_) _ _ _) =
+    concatMap (\y -> allStencils (stencil {middle = [y]})) (middle stencil)
+allStencils stencil@(Stencil _ _ _ _ (bl1:_:_) _ _) =
+    concatMap (\y -> allStencils (stencil {bottomLeft = [y]})) (bottomLeft stencil)
+allStencils stencil@(Stencil _ _ _ _ _ (br:_:_) _) =
+    concatMap (\y -> allStencils (stencil {bottomRight = [y]})) (bottomRight stencil)
+allStencils stencil@(Stencil _ _ _ _ _ _ (b:_:_)) =
+    concatMap (\y -> allStencils (stencil {bottom = [y]})) (bottom stencil)
+
+validStencil stencil@(Stencil [t] [l] [r] [m] [bl] [br] [b]) = all (\grp -> 1 == (length grp)) ((group . sort) [t,l,r,m,bl,br,b])
+
+stencilWorks (Stencil [t] [l] [r] [m] [bl] [br] [b]) word
+    | is1 word && (all (`elem` word) [r, br]) = True
+    | is4 word && (all (`elem` word) [l, r, m, br]) = True
+    | is7 word && (all (`elem` word) [t, r, br]) = True
+    | is8 word = True
+    | (length word) == 6 && (all (`elem` word) [t, l, r, bl, br, b]) = True
+    | (length word) == 6 && (all (`elem` word) [t, l, m, bl, br, b]) = True
+    | (length word) == 6 && (all (`elem` word) [t, l, r, m, br, b]) = True
+    | (length word) == 5 && (all (`elem` word) [t, r, m, bl, b]) = True
+    | (length word) == 5 && (all (`elem` word) [t, r, m, br, b]) = True
+    | (length word) == 5 && (all (`elem` word) [t, l, m, br, b]) = True
+    | otherwise = False
+stencilWorks stencil word = trace (show stencil) False
+
+stencilWorksForLine values stencil = all (stencilWorks stencil) values
 
 finalizeStencil :: Stencil -> Stencil
 finalizeStencil stencil = stencil {
@@ -83,28 +117,38 @@ verifyStencil :: Stencil -> Bool
 verifyStencil (Stencil [t] [l] [r] [m] [bl] [br] [b]) = False
 verifyStencil _ = True
 
+checkAgainstStencil :: String -> Stencil -> Int
+checkAgainstStencil word (Stencil [t] [l] [r] [m] [bl] [br] [b])
+    | is1 word = 1
+    | is4 word = 4
+    | is7 word = 7
+    | is8 word = 8
+    | (length word) == 6 && (all (`elem` word) [t, l, r, bl, br, b]) = 0
+    | (length word) == 6 && (all (`elem` word) [t, l, m, bl, br, b]) = 6
+    | (length word) == 6 && (all (`elem` word) [t, l, r, m, br, b]) = 9
+    | (length word) == 5 && (all (`elem` word) [t, r, m, bl, b]) = 2
+    | (length word) == 5 && (all (`elem` word) [t, r, m, br, b]) = 3
+    | (length word) == 5 && (all (`elem` word) [t, l, m, br, b]) = 5
+
+
 main :: IO ()
 main = do
     (fileName:_) <- getArgs
     contents <- readFile fileName
     let parsedContents = (map (splitNote . words) . lines) contents
 
-    let num1s = sum $ map (length . filter is1 . snd) parsedContents
-    let num4s = sum $ map (length . filter is4 . snd) parsedContents
-    let num7s = sum $ map (length . filter is7 . snd) parsedContents
-    let num8s = sum $ map (length . filter is8 . snd) parsedContents
+    -- let num1s = sum $ map (length . filter is1 . snd) parsedContents
+    -- let num4s = sum $ map (length . filter is4 . snd) parsedContents
+    -- let num7s = sum $ map (length . filter is7 . snd) parsedContents
+    -- let num8s = sum $ map (length . filter is8 . snd) parsedContents
 
-    let ws = map (sortOn length . fst) parsedContents
+    let decodeLines = map (sortOn length . fst) parsedContents
+    let codeLines = map snd parsedContents
 
+    let stencilForLines = map (allStencils . (foldl filterStencil emptyStencil)) decoders
 
-    let stencils = map (finalizeStencil . foldl filterStencil emptyStencil) ws
-    print $ stencils
+    let stencils = map (allStencils . (foldl filterStencil emptyStencil)) decoders
+    let stencilForLines = map (\decoder -> filter (stencilWorksForLine decoder) stencils) decodeLines
+    print $ head stencilForLines
+    -- print $ uncurry checkAgainstStencil $ head $ map head $ (map (\(code, stencil) -> map (\c -> (c, stencil)) code) $ zip codes stencils)
 
-    -- let largestValue = maximum parsedContents
-
-    -- print $ (minimum . map (calculateCost constantRateCost parsedContents)) [0..largestValue]
-    -- print $ (minimum . map (calculateCost linearRateCost parsedContents)) [0..largestValue]
-
-    -- Taking the median for pt1/mean for pt2 is the optimized version of the bruteforce approach above
-    -- print $ calculateCost constantRateCost parsedContents (median parsedContents)
-    -- print $ calculateCost linearRateCost parsedContents (mean parsedContents)
